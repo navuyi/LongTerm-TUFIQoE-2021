4b2513697536dd7bfcd5ca0b76fef27c7e176835
Rafal Figlus <rafal_figlus@wp.pl>
2021-08-30 16:04:49 +0200

    react navigation setup

--- App.js
+++ App.js
@@ -1,21 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+
+import Home from './screens/Home';
+import MobileTransferDecision from './screens/MobileTransferDecision';
+
+function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+  const Stack = createNativeStackNavigator()
+
+  /*// This is code responsible for displaying splash screen as long as app is loading/fetching external data
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync(); // <-- remain splash screen visible
+       
+        // Place for fetching data and async operations, before starting the app
+        await new Promise(resolve => setTimeout(resolve, 2000)); // <-- dummy delay
+
+
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+  */
+
+  const style = { 
+    flex: 1, 
+    alignItems: 'center', 
+    justifyContent: 'center',
+    backgroundColor: "#222222"
+  }
+
+
+  
+
+
   return (
-    <View style={styles.container}>
-      <Text> Hello world! </Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="MobileTransferDecision">
+        <Stack.Screen 
+          name="MobileTransferDecision" 
+          component={MobileTransferDecision} 
+          options={
+            {title: "Dopuszczać transfer komórkowy?"}
+          }
+        />
+        <Stack.Screen 
+          name="Home" 
+          component={Home}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  ); 
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
---
+++ screens/Home.js
@@ -0,0 +1,20 @@
+
+import { Text, View } from "react-native"
+import React from "react"
+
+const Home = () => {
+    const style={
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
+    }
+    return(
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Home Screen</Text>
+        </View>
+    )
+   
+}
+
+
+export default Home
